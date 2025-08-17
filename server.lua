6e1709e968ec1dc5a4f09143d2109f9149015064
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-17 06:55:54 -0300

    creating request

--- server.lua
+++ server.lua
@@ -6,11 +6,28 @@ local function api_handler(request)
   -- Process the request here
    if request.route == "/payments" then 
      local entries = request.read_json_body(400)
-     local url = ""
+     local decided = dtw.load_file("url.txt")
+     local decided_url = DEFAULT_URL
+     local decided_path = "./data/default"
+     if decided == "2" then 
+        decided_url = FALLBACK_URL
+        decided_path = "./data/fallback"
+     end
+     
+     local requisition = luabear.fetch({
+        url = decided_url.."/payments",
+        method = "POST",
+        body = entries
+     })
+     if requisition.status == 200 then
+        local absolute_time = dtw.get_absolute_time()
+        local path = decided_path.."/"..absolute_time.seconds.."_"..absolute_time.nanoseconds
+        dtw.write_file(path,tostring(entries.amount))
+     end
 
    end 
 
-
+   return "AQUI TEM CORAGEM"
 end
 
 serjao.server(3000, 5000, api_handler)
