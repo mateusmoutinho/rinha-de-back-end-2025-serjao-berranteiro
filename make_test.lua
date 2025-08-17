70235dcfc81fe9fb59abf8148629f8dd13ad6001
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-17 08:08:09 -0300

    att

--- make_test.lua
+++ make_test.lua
@@ -14,34 +14,34 @@ clpr.add_main(function()
     clpr.start_action(server)
     
     local required_files = {
-    {
-        url ="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/payment-processor/init.sql",
-        file="init.sql"
-    },
-    {
-        url="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/payment-processor/docker-compose.yml",
-        file="payments.yaml"
-    },
-    {url="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/rinha-test/rinha.js",
-     file="rinha.js"
-    },
-    {url="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/rinha-test/requests.js",
-     file="requests.js"
-    },
-}
-for i=1,#required_files do
-    local current = required_files[i]
-    if not dtw.isfile(current.file) then
-        print("Downloading "..current.url.." to "..current.file)
-        local cmd = string.format("curl -L %s -o %s", current.url, current.file)
-        os.execute(cmd)
+        {
+            url ="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/payment-processor/init.sql",
+            file="init.sql"
+        },
+        {
+            url="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/payment-processor/docker-compose.yml",
+            file="payments.yaml"
+        },
+        {url="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/rinha-test/rinha.js",
+        file="rinha.js"
+        },
+        {url="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/rinha-test/requests.js",
+        file="requests.js"
+        },
+    }   
+    for i=1,#required_files do
+        local current = required_files[i]
+        if not dtw.isfile(current.file) then
+            print("Downloading "..current.url.." to "..current.file)
+            local cmd = string.format("curl -L %s -o %s", current.url, current.file)
+            os.execute(cmd)
+        end
     end
-end
 
-os.execute("docker compose -f payments.yaml down -v")
-os.execute("docker compose -f payments.yaml up -d")
+    os.execute("docker compose -f payments.yaml down -v")
+    os.execute("docker compose -f payments.yaml up -d")
 
-os.execute("k6 run rinha.js")
+    os.execute("k6 run rinha.js")
 
 end )
 
