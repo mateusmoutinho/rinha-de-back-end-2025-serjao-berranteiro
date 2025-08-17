39c32cc7eb3045ab3e8c0ffaeb1917d837bb2212
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-17 08:34:16 -0300

    att

--- make_test.lua
+++ make_test.lua
@@ -38,4 +38,5 @@ clpr.add_main(function ()
     os.execute("docker compose -f payments.yaml up -d")
     os.execute("sleep 5") -- Wait for the server to be fully up
     os.execute("k6 run -e MAX_REQUESTS=50 rinha.js")
+    while true do end 
 end)
