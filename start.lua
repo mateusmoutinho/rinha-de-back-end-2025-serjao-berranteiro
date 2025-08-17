bf653bf84394ad6f041aa98f000c464190ab0758
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-17 08:10:01 -0300

    att

---
+++ start.lua
@@ -0,0 +1,31 @@
+
+
+dtw.remove_any("data")
+
+local required_files = {
+    {
+        url ="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/payment-processor/init.sql",
+        file="init.sql"
+    },
+    {
+        url="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/payment-processor/docker-compose.yml",
+        file="payments.yaml"
+    },
+    {url="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/rinha-test/rinha.js",
+    file="rinha.js"
+    },
+    {url="https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend-2025/refs/heads/main/rinha-test/requests.js",
+    file="requests.js"
+    },
+}   
+for i=1,#required_files do
+    local current = required_files[i]
+    if not dtw.isfile(current.file) then
+        print("Downloading "..current.url.." to "..current.file)
+        local cmd = string.format("curl -L %s -o %s", current.url, current.file)
+        os.execute(cmd)
+    end
+end
+
+os.execute("docker compose -f payments.yaml down -v")
+os.execute("docker compose -f payments.yaml up -d")
