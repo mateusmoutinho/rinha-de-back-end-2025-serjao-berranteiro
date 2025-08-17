4ce012c0bbf8d8f7bd46bf757bc0f8e39b8da336
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-17 11:21:32 -0300

    att

--- server.lua
+++ server.lua
@@ -3,7 +3,7 @@ FALLBACK_URL ="http://localhost:8002"
 
 
 set_server.max_queue = 1 
-set_server.max_request = 10000
+set_server.max_request = 10
 set_server.function_timeout = 1
 set_server.client_timeout = 100
 
