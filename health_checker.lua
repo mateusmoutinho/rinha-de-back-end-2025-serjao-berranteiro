a14333cbbca2e281177b68e2e553450119085e9b
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-17 20:24:01 -0300

    att

--- health_checker.lua
+++ health_checker.lua
@@ -15,11 +15,10 @@ local get_status = clpr.add_action({
 
 clpr.add_main(function ()
         while true do
-            print("pegou aqui")
             local default_status = clpr.start_action(get_status, {url=DEFAULT_URL})
             local fallback_status = clpr.start_action(get_status, {url=FALLBACK_URL})
-            default_status.wait(10, function() os.execute("sleep 1") end)
-            fallback_status.wait(10, function() os.execute("sleep 1") end)
+            os.execute("sleep 5") -- Wait for the services to respond
+
            
             local response_default = default_status.get_result()
             local response_fallback = fallback_status.get_result()
