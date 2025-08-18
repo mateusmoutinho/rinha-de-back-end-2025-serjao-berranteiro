b9c84f8f6b54b8801eb48ae6a4adc6e32678460d
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-17 21:22:58 -0300

    att

--- make_test.lua
+++ make_test.lua
@@ -35,9 +35,10 @@ os.execute("sleep 20") -- Wait for the server to be fully up
 
 -- Start new docker-compose scenario first
 os.execute("docker compose -f docker-compose.yaml down -v")
-os.execute("docker compose -f docker-compose.yaml build --no-cache") 
+os.execute("docker compose -f docker-compose.yaml build ") 
 os.execute("docker compose -f docker-compose.yaml up -d")
 os.execute("sleep 20") -- Wait for the new services to be fully up
+while true do end 
 
 os.execute("k6 run rinha.js")
 os.execute("docker compose -f payments.yaml down -v")
